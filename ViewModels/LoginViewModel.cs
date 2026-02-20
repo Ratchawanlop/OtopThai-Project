8383421441ec68b64573103eee217c370acb2934
Ratchawanlop <ratchawanlop.nao@spumail.net>
2026-02-20 19:56:30 +0700

    error login path

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.ViewModels;
 
 public class LoginViewModel
 {
-    public string Phone_number { get; set; } = string.Empty;
-    public string Password_hash { get; set; } = string.Empty;
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "กรุณากรอกเบอร์โทรศัพท์")]
+        [RegularExpression(@"^0[0-9]{9}$", ErrorMessage = "รูปแบบเบอร์โทรศัพท์ไม่ถูกต้อง (เช่น 0812345678)")]
+        [Display(Name = "เบอร์โทรศัพท์")]
+        public string Phone_number { get; set; }
+
+        [Required(ErrorMessage = "กรุณากรอกรหัสผ่าน")]
+        [DataType(DataType.Password)]
+        [Display(Name = "รหัสผ่าน")]
+        // ใช้ชื่อตัวแปร Password ตรงๆ ใน Form ได้เลยครับ ตอนเซฟ/เช็คใน Controller ค่อยเอาไปเทียบกับ Password_hash ใน DB
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
 }
 
 public class RegisterViewModel
--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71112d2ca6370bf5ce1bd4b97036cad9ecf72fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203dfc32d47398ce228913d09e251a3d59248682")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
