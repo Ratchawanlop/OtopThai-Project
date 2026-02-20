203dfc32d47398ce228913d09e251a3d59248682
Ratchawanlop <ratchawanlop.nao@spumail.net>
2026-02-20 19:32:42 +0700

    fix layout

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca71320e6f531cf3fc5e689a4c7571475edd467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71112d2ca6370bf5ce1bd4b97036cad9ecf72fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
